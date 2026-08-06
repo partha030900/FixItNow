b3f29a84627262bdd0fc5e77bd08e67c73096d6c
partha030900 <partha030900@gmail.com>
2026-08-06 15:46:45 +0600

    Environment variables connected

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,9 +14,9 @@ export default {
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
   jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!,
-  stripe_secret_key: process.env.STRIPE_SECRET_KEY!,
-  stripe_webhook_secret: process.env.STRIPE_WEBHOOK_SECRET!,
   admin_email: process.env.ADMIN_EMAIL!,
   admin_password: process.env.ADMIN_PASSWORD!,
+  stripe_secret_key: process.env.STRIPE_SECRET_KEY!,
+  stripe_webhook_secret: process.env.STRIPE_WEBHOOK_SECRET!,
   node_env: process.env.NODE_ENV,
 };
